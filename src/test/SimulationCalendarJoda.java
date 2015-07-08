276fb8e33aa709e6118ea7fd1a12dc106dd1e5be
crygiova <giovanc1@T41000-LR005.org.aalto.fi>
2015-07-08 16:30:08 +0300

    Revert "Revert "first commit""
    This reverts commit 2bc9bc4cc3d1f877dad88a00a1ec4931dec4e6a3.

---
+++ src/test/SimulationCalendarJoda.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package test;
+
+import org.joda.time.DateTime;
+
+
+
+/**
+ * @author giovanc1
+ *
+ */
+public class SimulationCalendarJoda {
+	
+	private DateTime calendar = new DateTime();
+	
+
+}
